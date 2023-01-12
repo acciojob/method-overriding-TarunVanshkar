e1765e448332c7959325125375595f949ce2cefc
TarunVanshkar <tarunben33@gmail.com>
2023-01-12 23:17:15 +0530

    2nd attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -11,3 +11,17 @@ public class Main
   }
 }
 
+class A {
+    public String meth()
+    {
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A
+{
+    public String meth()
+    {
+        return "Method is overridden in Extendend class B";
+    }
+}
